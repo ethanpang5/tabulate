15fc5da12a600afe1192130782da251a92165ee4
Rishab Satyakaal <rishab.satyakaal@berkeley.edu>
2021-03-15 21:41:10 -0700

    minor bug fixes for open-all

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,23 @@ function openAllClasses() {
         window.open(links[i].getAttribute("href"), "_blank")
     }
 }
+
+function openAllFavorites() {
+    let links = document.getElementsByClassName("favorites-link")
+    for (let i=0; i < links.length; i++) {
+        window.open(links[i].getAttribute("href"), "_blank")
+    }
+}
+
+function addElement() {
+    /** incomplete  */
+    let div = document.createElement('div');
+    const link = "https://www.nba.com/"
+    div.setAttribute("href", link)
+    div.setAttribute("class", "favorites-link")
+    
+
+
+    document.getElementById("favorites").addEventListener()
+}
+
