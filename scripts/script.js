f23a36af0f17ec877e6c777040574d939ad4ec0e
Ethan Pang <ethanp5@berkeley.edu>
2021-03-18 18:44:29 -0700

    implement adding new website link

--- scripts/script.js
+++ scripts/script.js
@@ -12,15 +12,21 @@ function openAllFavorites() {
     }
 }
 
-function addElement() {
+function addElement(name, url) {
     /** incomplete  */
     let div = document.createElement('div');
     let a = document.createElement("a")
-    const link = "https://www.nba.com/"
-    a.setAttribute("href", link)
-    a.innerText = "NBA"
+    // const link = "https://www.nba.com/"
+    a.setAttribute("href", url)
+    a.setAttribute("target", "_blank")
+    a.innerText = name
     div.setAttribute("class", "favorites-link")
     div.appendChild(a)
     document.getElementById("favorites").appendChild(div)
 }
 
+function addWebsite() {
+    let name = document.getElementById("website-name").value
+    let url = document.getElementById("website-url").value
+    addElement(name, url)
+}
