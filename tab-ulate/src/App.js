4741021cea5e7ba8cc708790bc3b4e1f9aef4279
Charles Ming <charlesming@berkeley.edu>
2021-04-05 01:44:00 -0400

    fix navbar

--- tab-ulate/src/App.js
+++ tab-ulate/src/App.js
@@ -1,10 +1,13 @@
     /*global chrome*/
 import React, { useEffect, useState } from "react";
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
-import { NavbarComponent } from "./components/navbarComponent";
+import { NavbarComponent } from "./components/NavbarComponent";
 
 function App() {
+  console.log('new');
   const [recents, setRecents] = useState([]);
   useEffect(() => {
     chrome.history.search({text: '', maxResults: 10}, function(data) {
@@ -28,15 +31,15 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <>
       <NavbarComponent/>
-        <button onClick={handleClick}>Test</button>
+      <button onClick={handleClick}>Test</button>
         <ul>
           {recents.map((link) => (
             <li>{link}</li>
           ))}
         </ul>
-    </div>
+    </>
   );
 }
 
