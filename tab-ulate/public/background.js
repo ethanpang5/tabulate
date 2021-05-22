7a56e3723d2b3c0c43c71af15965d60970624a0b
Ethan Pang <ethanp5@berkeley.edu>
2021-05-21 19:35:29 -0700

    remove subfolder

--- tab-ulate/public/background.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line no-undef
-chrome.browserAction.onClicked.addListener(function(activeTab)
-{
-    // eslint-disable-next-line no-undef
-    chrome.tabs.create({ url: chrome.extension.getURL('index.html'), selected: true });
-});
